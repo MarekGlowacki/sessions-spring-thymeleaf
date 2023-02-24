747d04719189f34ca4f1525cd79f3aa95a4d00af
User <user@its.me>
2023-02-24 12:40:05 +0100

    Initial commit

---
+++ src/main/java/online/javafun/sessions/SessionsSpringThymeleafApplication.java
@@ -0,0 +1,10 @@
+package online.javafun.sessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionsSpringThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionsSpringThymeleafApplication.class, args
---
+++ src/test/java/online/javafun/sessions/SessionsSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.sessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionsSpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
